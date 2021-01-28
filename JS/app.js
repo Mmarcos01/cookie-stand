8e8cc337ac17c75a841afcd8042c42bdc63f92e5
Marie Marcos <marie.marcos25@gmail.com>
2021-01-27 19:16:16 -0800

    framework for lab 6

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
 console.log('hello world');
+let myContainer = document.getElementById('container');
+// let seattleList = document.getElementById('list');
 
+console.log(myContainer);
 
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2am', '3am', '4am', '5am', '6pm', '7pm'];
+
+// let cookies = ['16', '20', '35', '48', '56', '77', '93', '144', '119', '84', '61', '2361', '42', '57'];
+
+let seattle = {
+  name: 'Seattle',
+  minCusPerHour: 23,
+  maxCusPerHour: 65,
+  avgCookiesSoldPerCustomer: 6.3,
